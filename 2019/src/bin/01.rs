93500df5d2ab394e2b8ef8c5999a77fc6098a0b6
Jan Solanti <jhs@psonet.com>
2019-12-15 16:21:58 +0200

    Add 2019 / Day02, add top-level Cargo.toml for RLS

--- 2019/src/bin/01.rs
+++ 2019/src/bin/01.rs
@@ -4,7 +4,7 @@ fn main() {
     let masses = INPUT.lines().map(|l| l.parse::<i64>().unwrap()).collect::<Vec<i64>>();
 
     // Part 1
-    let sum: i64 = masses.iter().map(|x| (x / 3) - 2).sum();
+    let sum: i64 = masses.iter().map(fuel).sum();
 
     // Answer with my input was 3348430
     println!("Part 1) Total fuel: {}\n", sum);
@@ -12,16 +12,82 @@ fn main() {
 
 
     // Part 2
-    fn fuel_rec(x: &i64) -> i64 {
-        if *x > 0 {
-            let tmp = ((*x / 3 ) - 2).max(0);
-            tmp + fuel_rec(&tmp)
-        } else {
-            0
-        }
-    };
     let sum: i64 = masses.iter().map(fuel_rec).sum();
 
     // Answer with my input was 5019767
     println!("Part 2) Total fuel: {}\n", sum);
+}
+
+fn fuel(x: &i64) -> i64 {
+    (x / 3) - 2
+}
+
+fn fuel_rec(x: &i64) -> i64 {
+    if *x > 0 {
+        let tmp = ((*x / 3 ) - 2).max(0);
+        tmp + fuel_rec(&tmp)
+    } else {
+        0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{fuel, fuel_rec};
+
+    #[test]
+    fn day01_part1_example_1() {
+        let mass = 12;
+        let res = 2;
+        let f = fuel(&mass);
+        assert_eq!(f, res);
+    }
+
+    #[test]
+    fn day01_part1_example_2() {
+        let mass = 14;
+        let res = 2;
+        let f = fuel(&mass);
+        assert_eq!(f, res);
+    }
+
+    #[test]
+    fn day01_part1_example_3() {
+        let mass = 1969;
+        let res = 654;
+        let f = fuel(&mass);
+        assert_eq!(f, res);
+    }
+
+    #[test]
+    fn day01_part1_example_4() {
+        let mass = 100756;
+        let res = 33583;
+        let f = fuel(&mass);
+        assert_eq!(f, res);
+    }
+
+    #[test]
+    fn day01_part2_example_1() {
+        let mass = 14;
+        let res = 2;
+        let f = fuel_rec(&mass);
+        assert_eq!(f, res);
+    }
+
+    #[test]
+    fn day01_part2_example_2() {
+        let mass = 1969;
+        let res = 966;
+        let f = fuel_rec(&mass);
+        assert_eq!(f, res);
+    }
+
+    #[test]
+    fn day01_part2_example_3() {
+        let mass = 100756;
+        let res = 50346;
+        let f = fuel_rec(&mass);
+        assert_eq!(f, res);
+    }
 }
---
+++ 2019/src/bin/02.rs
@@ -0,0 +1,109 @@
+const INPUT: &str = include_str!("input/02.txt");
+
+fn main() {
+    let program = INPUT.trim().split_terminator(",").map(|l| l.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+
+    // Part 1
+    let mut ram = program.clone();
+    // "restore program to its state before the 1202 alarm"
+    ram[1] = 12;
+    ram[2] = 2;
+    exec_intcode(&mut ram[..]);
+
+    // Answer with my input was 4714701
+    println!("Part 1) Value at position 0: {}\n", ram[0]);
+
+
+
+    // Part 2
+    let mut result: i64 = 0;
+    let target: i64 = 19690720;
+    let mut noun: i64 = -1;
+    let mut verb: i64 = -1;
+    while result != target && noun < 99 {
+        noun += 1;
+        verb = -1;
+
+        while result != target && verb < 99 {
+            verb += 1;
+
+            ram = program.clone();
+            ram[1] = noun;
+            ram[2] = verb;
+            exec_intcode(&mut ram);
+            result = ram[0];
+        }
+    }
+
+    // Answer with my input was 5121
+    println!("Part 2) 100*noun({}) + verb({}) = {}", noun, verb, 100*noun+verb);
+}
+
+fn exec_intcode(ram: &mut [i64]) {
+    let mut pc = 0;
+    let mut halt = false;
+    while !halt {
+        let op = ram[pc];
+        match op {
+            1 => /* ADD a, b, dst */ {
+                let a = ram[(pc+1) as usize] as usize;
+                let b = ram[(pc+2) as usize] as usize;
+                let dst = ram[(pc+3) as usize] as usize;
+                ram[dst] = ram[a] + ram[b];
+            },
+            2 => /* MUL a, b, dst */ {
+                let a = ram[(pc+1) as usize] as usize;
+                let b = ram[(pc+2) as usize] as usize;
+                let dst = ram[(pc+3) as usize] as usize;
+                ram[dst] = ram[a] * ram[b];
+            },
+            99 => /* HALT */ {
+                halt = true
+            },
+            x => /* ERROR */ {
+                panic!("INVALID OPCODE: {}, AT ADDR: {}", x, pc);
+            }
+        }
+        pc += 4;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::exec_intcode;
+
+    #[test]
+    fn day02_part1_example_0() {
+        let mut program = vec![1,9,10,3,2,3,11,0,99,30,40,50];
+        exec_intcode(&mut program[..]);
+        assert_eq!(program, vec![3500,9,10,70,2,3,11,0,99,30,40,50])
+    }
+
+    #[test]
+    fn day02_part1_example_1() {
+        let mut program = vec![1,0,0,0,99];
+        exec_intcode(&mut program[..]);
+        assert_eq!(program, vec![2,0,0,0,99]);
+    }
+
+    #[test]
+    fn day02_part1_example_2() {
+        let mut program = vec![2,3,0,3,99];
+        exec_intcode(&mut program[..]);
+        assert_eq!(program, vec![2,3,0,6,99]);
+    }
+
+    #[test]
+    fn day02_part1_example_3() {
+        let mut program = vec![2,4,4,5,99,0];
+        exec_intcode(&mut program[..]);
+        assert_eq!(program, vec![2,4,4,5,99,9801]);
+    }
+
+    #[test]
+    fn day02_part1_example_4() {
+        let mut program = vec![1,1,1,4,99,5,6,0,99];
+        exec_intcode(&mut program[..]);
+        assert_eq!(program, vec![30,1,1,4,2,5,6,0,99]);
+    }
+}
